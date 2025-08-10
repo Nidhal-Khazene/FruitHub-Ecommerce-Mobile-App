19a1642239f1bf13ae6cc72626c70979320b325a
nidhal khazene <nidhal.khazene@gmail.com>
2025-08-10 11:29:31 +0100

    add spalsh view structure

---
+++ lib/core/helper_functions/on_generate_route.dart
@@ -0,0 +1,11 @@
+import 'package:ecommerce_app/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case SplashView.splashViewRoute:
+      return MaterialPageRoute(builder: (context) => const SplashView());
+    default:
+      return MaterialPageRoute(builder: (context) => const Scaffold());
+  }
+}
