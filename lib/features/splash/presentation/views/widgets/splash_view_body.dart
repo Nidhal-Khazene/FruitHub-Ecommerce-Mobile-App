73b6de6117089a3e8efc16f59cc2b96f4521dfe1
nidhal khazene <nidhal.khazene@gmail.com>
2025-08-10 18:17:48 +0100

    splash view logic done

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,21 @@
 import 'package:ecommerce_app/core/utils/assets.dart';
+import 'package:ecommerce_app/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    executeNavigation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +30,16 @@ class SplashViewBody extends StatelessWidget {
         SvgPicture.asset(Assets.assetsImagesLogoSplash),
         SvgPicture.asset(Assets.assetsImagesCirclesSplash, fit: BoxFit.fill),
       ],
+    );
+  }
+
+  void executeNavigation() {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () => Navigator.pushReplacementNamed(
+        context,
+        OnBoardingView.onBoardingViewRoute,
+      ),
     );
   }
 }
