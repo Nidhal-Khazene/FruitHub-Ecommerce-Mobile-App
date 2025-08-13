f1743336f24ffb8b013c72dccf3149c74dc2d951
nidhal khazene <nidhal.khazene@gmail.com>
2025-08-13 10:57:07 +0100

    add login view body structure

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,11 +1,13 @@
+import 'package:ecommerce_app/features/auth/presentation/views/widgets/login_view_body.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
+
   static const routeName = "/LoginView";
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(body: LoginViewBody());
   }
 }
