e8ecd503401134d665c133fc5f3f191fc94dd19d
nidhal khazene <nidhal.khazene@gmail.com>
2025-08-18 10:28:32 +0100

    add firebase and fix some issues

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'package:ecommerce_app/core/helper_functions/on_generate_routes.dart';
 import 'package:ecommerce_app/core/services/shared_preferences_singleton.dart';
 import 'package:ecommerce_app/core/utils/colors.dart';
 import 'package:ecommerce_app/features/splash/presentation/views/splash_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'firebase_options.dart';
 import 'generated/l10n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await SharedPreferencesSingleton.init();
   runApp(const FruitHubApp());
 }
