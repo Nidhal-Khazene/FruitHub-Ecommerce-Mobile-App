16a6272ec0746adcac6598cfa34bf5b33bfd8d92
nidhal khazene <nidhal.khazene@gmail.com>
2025-08-19 10:45:30 +0100

    create service locator

--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,17 +1,25 @@
+import 'package:ecommerce_app/core/services/service_locator.dart';
+import 'package:ecommerce_app/features/auth/domain/repos/auth_repo.dart';
+import 'package:ecommerce_app/features/auth/presentation/manager/cubits/sign_up_cubit.dart';
 import 'package:ecommerce_app/features/auth/presentation/views/widgets/signup_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/widgets/custom_app_bar.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
+
   static const String routeName = "signUpView";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, title: 'حساب جديد'),
-      body: const SignUpViewBody(),
+    return BlocProvider<SignUpCubit>(
+      create: (context) => SignUpCubit(getIt<AuthRepo>()),
+      child: Scaffold(
+        appBar: buildAppBar(context, title: 'حساب جديد'),
+        body: const SignUpViewBody(),
+      ),
     );
   }
 }
