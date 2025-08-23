727ab32e41ef53f27943194ca3ba4ae029a5f1f5
nidhal khazene <nidhal.khazene@gmail.com>
2025-08-23 17:55:53 +0100

    provide loginCubit

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,6 +1,10 @@
+import 'package:ecommerce_app/core/services/service_locator.dart';
 import 'package:ecommerce_app/core/widgets/custom_app_bar.dart';
+import 'package:ecommerce_app/features/auth/domain/repos/auth_repo.dart';
+import 'package:ecommerce_app/features/auth/presentation/manager/cubits/log_in_cubit/login_cubit.dart';
 import 'package:ecommerce_app/features/auth/presentation/views/widgets/login_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
@@ -9,9 +13,12 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, title: "تسجيل دخول"),
-      body: const LoginViewBody(),
+    return BlocProvider<LoginCubit>(
+      create: (context) => LoginCubit(getIt<AuthRepo>()),
+      child: Scaffold(
+        appBar: buildAppBar(context, title: "تسجيل دخول"),
+        body: const LoginViewBody(),
+      ),
     );
   }
 }
