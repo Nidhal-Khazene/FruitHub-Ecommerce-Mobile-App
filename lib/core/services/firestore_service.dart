0a729f1b430b6f6c9931bd1149febea1495acfe8
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-04 14:04:02 +0100

    implement addUser method in firestore service

--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,1 +1,14 @@
-class FilestoreService{}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce_app/core/services/database_service.dart';
+
+class FilestoreService extends DatabaseService {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  @override
+  Future<void> addData({
+    required String path,
+    required Map<String, dynamic> data,
+  }) async {
+    await db.collection(path).add(data);
+  }
+}
