10cab180bac4e3fd3e2e0da1f7e36d69040fdee3
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-09 11:37:36 +0100

    add getUserData to database service

--- lib/core/services/database_service.dart
+++ lib/core/services/database_service.dart
@@ -1,6 +1,9 @@
+import 'package:ecommerce_app/features/auth/domain/entities/user_entity.dart';
+
 abstract class DatabaseService {
   Future<void> addData({
     required String path,
     required Map<String, dynamic> data,
   });
+  Future<UserEntity> getUserData({required String uId, required String path});
 }
