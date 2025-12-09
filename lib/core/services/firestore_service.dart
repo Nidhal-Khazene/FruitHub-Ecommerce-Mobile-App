e1facfe9396818c444209b7623b7ec16effc0cbd
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-09 11:46:37 +0100

    implement getUserData in firestoreService

--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce_app/core/services/database_service.dart';
+import 'package:ecommerce_app/features/auth/data/models/user_model.dart';
+import 'package:ecommerce_app/features/auth/domain/entities/user_entity.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FirestoreService extends DatabaseService {
   FirebaseFirestore db = FirebaseFirestore.instance;
@@ -10,5 +13,14 @@ class FirestoreService extends DatabaseService {
     required Map<String, dynamic> data,
   }) async {
     await db.collection(path).add(data);
+  }
+
+  @override
+  Future<UserEntity> getUserData({
+    required String uId,
+    required String path,
+  }) async {
+    var data = await db.collection(path).doc(uId).get();
+    return UserModel.fromJson(data.data()!);
   }
 }
