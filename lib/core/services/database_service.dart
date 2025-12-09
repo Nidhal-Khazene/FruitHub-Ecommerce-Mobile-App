f7c3c019ab5a7e1b62715bc817190cc4213a4e75
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-09 11:59:30 +0100

    change getUserData to getData in database service

--- lib/core/services/database_service.dart
+++ lib/core/services/database_service.dart
@@ -1,9 +1,10 @@
-import 'package:ecommerce_app/features/auth/domain/entities/user_entity.dart';
-
 abstract class DatabaseService {
   Future<void> addData({
     required String path,
     required Map<String, dynamic> data,
   });
-  Future<UserEntity> getUserData({required String uId, required String path});
+  Future<Map<String, dynamic>> getData({
+    required String documentsId,
+    required String path,
+  });
 }
