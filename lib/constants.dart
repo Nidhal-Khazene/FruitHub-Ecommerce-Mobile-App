c2c36eb992f8251f6b5d0973ae1724558e332ade
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-10 12:13:41 +0100

    add kPrimaryScreenPadding

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
 const double kHorizontalPadding = 16.0;
 const double kSizedBoxHeight16 = 16.0;
 const double kSizedBoxHeight33 = 33.0;
 const String kIsOnBoardingViewSeen = 'IsOnBoardingViewSeen';
+const EdgeInsetsGeometry kPrimaryScreenPadding = EdgeInsets.only(
+  top: 16,
+  right: 16,
+  left: 16,
+);
