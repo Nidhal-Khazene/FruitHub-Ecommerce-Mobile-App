4a39c02ba26a027ce6091d496bf99807d0930bd5
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-14 15:07:55 +0100

    add custom search bar

---
+++ lib/core/widgets/custom_search_bar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../utils/styles.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  const CustomSearchBar({
+    super.key,
+    required this.hintText,
+    this.suffixIcon,
+    this.suffixIconColor,
+    this.prefixIcon,
+    this.prefixIconColor,
+    this.fillColor,
+    this.borderWidth,
+  });
+  final String hintText;
+  final Widget? suffixIcon;
+  final Color? suffixIconColor;
+  final Widget? prefixIcon;
+  final Color? prefixIconColor;
+  final Color? fillColor;
+  final double? borderWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: fillColor ?? const Color(0xffF9FAFA),
+        border: buildOutlineInputBorder(),
+        hint: Text(
+          hintText,
+          style: TextStyles.bold13.copyWith(color: const Color(0xFF949D9E)),
+        ),
+        prefixIcon: prefixIcon,
+        prefixIconColor: prefixIconColor,
+        suffixIcon: suffixIcon,
+        suffixIconColor: suffixIconColor ?? const Color(0xFFC9CECE),
+        enabledBorder: buildOutlineInputBorder(borderWidth),
+        focusedBorder: buildOutlineInputBorder(borderWidth),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildOutlineInputBorder([double? borderWidth]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(4),
+      borderSide: BorderSide(
+        width: borderWidth ?? 1,
+        color: const Color(0xFFE6E9E9),
+      ),
+    );
+  }
+}
