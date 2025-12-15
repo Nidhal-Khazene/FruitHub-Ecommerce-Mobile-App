7b9e5edf6ecb7fb1a64e96485eb7f5ceb64824ad
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-15 11:25:28 +0100

    add carousel view to featured section

--- lib/features/home/presentation/views/widgets/featured_item.dart
+++ lib/features/home/presentation/views/widgets/featured_item.dart
@@ -14,44 +14,47 @@ class FeaturedItem extends StatelessWidget {
       aspectRatio: 2,
       child: ClipRRect(
         borderRadius: const BorderRadius.all(Radius.circular(4)),
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: Image.asset(
-                Assets.assetsImagesFeaturedImage1,
-                fit: BoxFit.cover,
+        child: SizedBox(
+          height: 200,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: Image.asset(
+                  Assets.assetsImagesFeaturedImage1,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Positioned(
-              right: 24,
-              top: 35,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "عروض العيد",
-                    style: TextStyles.regular13.copyWith(color: Colors.white),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    "خصم 25%",
-                    style: TextStyles.bold19.copyWith(color: Colors.white),
-                  ),
-                  const SizedBox(height: 7),
-                  CustomButton(
-                    borderRadiusValue: 4,
-                    width: 116,
-                    height: 34,
-                    text: "تسوق الان",
-                    textStyle: TextStyles.bold13.copyWith(
-                      color: ColorData.kPrimaryColor,
+              Positioned(
+                right: 24,
+                top: 35,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "عروض العيد",
+                      style: TextStyles.regular13.copyWith(color: Colors.white),
                     ),
-                    backgroundColor: Colors.white,
-                  ),
-                ],
+                    const SizedBox(height: 8),
+                    Text(
+                      "خصم 25%",
+                      style: TextStyles.bold19.copyWith(color: Colors.white),
+                    ),
+                    const SizedBox(height: 7),
+                    CustomButton(
+                      borderRadiusValue: 4,
+                      width: 116,
+                      height: 34,
+                      text: "تسوق الان",
+                      textStyle: TextStyles.bold13.copyWith(
+                        color: ColorData.kPrimaryColor,
+                      ),
+                      backgroundColor: Colors.white,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/features/home/presentation/views/widgets/featured_section.dart
+++ lib/features/home/presentation/views/widgets/featured_section.dart
@@ -1,7 +1,4 @@
-import 'package:ecommerce_app/core/utils/assets.dart';
-import 'package:ecommerce_app/core/utils/colors.dart';
-import 'package:ecommerce_app/core/utils/styles.dart';
-import 'package:ecommerce_app/core/widgets/custom_button.dart';
+import 'package:ecommerce_app/features/home/presentation/views/widgets/featured_item.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedSection extends StatelessWidget {
@@ -9,50 +6,20 @@ class FeaturedSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2,
-      child: ClipRRect(
-        borderRadius: const BorderRadius.all(Radius.circular(4)),
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: Image.asset(
-                Assets.assetsImagesFeaturedImage1,
-                fit: BoxFit.cover,
-              ),
+    return Column(
+      children: [
+        SizedBox(
+          height: 200,
+          child: CarouselView(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(4),
             ),
-            Positioned(
-              right: 24,
-              top: 35,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "عروض العيد",
-                    style: TextStyles.regular13.copyWith(color: Colors.white),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    "خصم 25%",
-                    style: TextStyles.bold19.copyWith(color: Colors.white),
-                  ),
-                  const SizedBox(height: 7),
-                  CustomButton(
-                    borderRadiusValue: 4,
-                    width: 116,
-                    height: 34,
-                    text: "تسوق الان",
-                    textStyle: TextStyles.bold13.copyWith(
-                      color: ColorData.kPrimaryColor,
-                    ),
-                    backgroundColor: Colors.white,
-                  ),
-                ],
-              ),
-            ),
-          ],
+            itemSnapping: true,
+            itemExtent: MediaQuery.sizeOf(context).width,
+            children: List.generate(3, (index) => const FeaturedItem()),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
