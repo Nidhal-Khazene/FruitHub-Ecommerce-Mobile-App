29702332218592fe905ab567691e99247206b355
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-17 11:44:20 +0100

    add bottom nav bar entity

---
+++ lib/features/home/domain/entities/bottom_nav_bar_entity.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+
+class BottomNavBarEntity {
+  final IconData activeIcon, inActiveIcon;
+  final String iconTitle;
+
+  BottomNavBarEntity({
+    required this.activeIcon,
+    required this.inActiveIcon,
+    required this.iconTitle,
+  });
+
+  List<BottomNavBarEntity> get getBottomNavBarEntityList => [
+    BottomNavBarEntity(
+      activeIcon: Iconsax.home,
+      inActiveIcon: Iconsax.home_copy,
+      iconTitle: "الرئيسية",
+    ),
+    BottomNavBarEntity(
+      activeIcon: Iconsax.element_3,
+      inActiveIcon: Iconsax.element_3_copy,
+      iconTitle: "المنتجات",
+    ),
+    BottomNavBarEntity(
+      activeIcon: Iconsax.shopping_cart,
+      inActiveIcon: Iconsax.shopping_cart_copy,
+      iconTitle: "سلة التسوق",
+    ),
+    BottomNavBarEntity(
+      activeIcon: Iconsax.user,
+      inActiveIcon: Iconsax.user_copy,
+      iconTitle: "حسابي",
+    ),
+  ];
+}
