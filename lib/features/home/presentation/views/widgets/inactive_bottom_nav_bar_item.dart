85a0cd6f915546e84da00fa9848d2c42471d7ef7
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-19 16:14:14 +0100

    add incative bottom nav bar item

---
+++ lib/features/home/presentation/views/widgets/inactive_bottom_nav_bar_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InactiveBottomNavBarItem extends StatelessWidget {
+  const InactiveBottomNavBarItem({super.key, required this.inActiveIcon});
+  final IconData inActiveIcon;
+  @override
+  Widget build(BuildContext context) {
+    return Icon(inActiveIcon);
+  }
+}
