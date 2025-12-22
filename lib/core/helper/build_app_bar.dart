9ad56a0cdd59cf1ae4bd21e7b92469973ac8f105
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-22 21:39:30 +0100

    add build app bar

---
+++ lib/core/helper/build_app_bar.dart
@@ -0,0 +1,39 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+import '../utils/styles.dart';
+import '../widgets/notification_container.dart';
+
+AppBar buildAppBar(BuildContext context, {required String title}) {
+  return AppBar(
+    automaticallyImplyLeading: false,
+    leadingWidth: 70,
+    leading: Padding(
+      padding: const EdgeInsets.only(right: 16),
+      child: Container(
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100),
+            side: const BorderSide(width: 1, color: Color(0xFFF1F1F5)),
+          ),
+        ),
+        child: Transform.rotate(
+          angle: math.pi,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(Icons.arrow_forward_ios, size: 22),
+          ),
+        ),
+      ),
+    ),
+    actionsPadding: const EdgeInsets.only(left: 16),
+    actions: [const NotificationContainer()],
+    elevation: 0,
+    backgroundColor: Colors.white,
+    centerTitle: true,
+    title: Text(title, style: TextStyles.bold19),
+  );
+}
