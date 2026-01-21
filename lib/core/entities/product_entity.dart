d2780f4f9288f7d8e32b0f295c50910f45e573c3
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-21 21:20:48 +0100

    add products entity and model also review model and entity

---
+++ lib/core/entities/product_entity.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import 'package:ecommerce_app/core/entities/review_entity.dart';
+
+class ProductEntity {
+  final String productName, productCode, productDescription;
+  String? urlImage;
+  final num productPrice;
+  final File fileImage;
+  final bool isFeatured;
+  final int expirationsMonth;
+  final bool isOrganic;
+  final int numberOfCalories;
+  final num avrRating;
+  final num ratingCount;
+  final int unitAmount;
+  final List<ReviewEntity> reviews;
+
+  ProductEntity({
+    required this.reviews,
+    this.avrRating = 0,
+    this.ratingCount = 0,
+    required this.expirationsMonth,
+    required this.isOrganic,
+    required this.numberOfCalories,
+    required this.unitAmount,
+    required this.productName,
+    required this.productCode,
+    required this.productDescription,
+    this.urlImage,
+    required this.productPrice,
+    required this.fileImage,
+    required this.isFeatured,
+  });
+}
---
+++ lib/core/models/product_model.dart
@@ -0,0 +1,71 @@
+import 'package:ecommerce_app/core/models/review_model.dart';
+
+import '../entities/product_entity.dart';
+
+class ProductModel {
+  final String productName;
+  final String productCode;
+  final String productDescription;
+  final num productPrice;
+  final bool isFeatured;
+  final String? urlImage;
+  final int expirationsMonth;
+  final bool isOrganic;
+  final int numberOfCalories;
+  final num avrRating;
+  final num ratingCount;
+  final int unitAmount;
+  final List<ReviewModel> reviews;
+
+  ProductModel({
+    required this.reviews,
+    this.avrRating = 0,
+    this.ratingCount = 0,
+    required this.expirationsMonth,
+    this.isOrganic = false,
+    required this.numberOfCalories,
+    required this.unitAmount,
+    required this.productName,
+    required this.productCode,
+    required this.productDescription,
+    required this.productPrice,
+    this.isFeatured = false,
+    this.urlImage,
+  });
+
+  factory ProductModel.fromEntity(ProductEntity entity) {
+    return ProductModel(
+      reviews: entity.reviews.map((e) => ReviewModel.fromEntity(e)).toList(),
+      productName: entity.productName,
+      productCode: entity.productCode,
+      productDescription: entity.productDescription,
+      productPrice: entity.productPrice,
+      isFeatured: entity.isFeatured,
+      urlImage: entity.urlImage,
+      expirationsMonth: entity.expirationsMonth,
+      isOrganic: entity.isOrganic,
+      numberOfCalories: entity.numberOfCalories,
+      unitAmount: entity.unitAmount,
+      avrRating: entity.avrRating,
+      ratingCount: entity.ratingCount,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "productName": productName,
+      "productCode": productCode,
+      "productDescription": productDescription,
+      "productPrice": productPrice,
+      "isFeatured": isFeatured,
+      "urlImage": urlImage,
+      "expirationsMonth": expirationsMonth,
+      "isOrganic": isOrganic,
+      "numberOfCalories": numberOfCalories,
+      "unitAmount": unitAmount,
+      "avrRating": avrRating,
+      "ratingCount": ratingCount,
+      "reviews": reviews.map((e) => e.toJson()).toList(),
+    };
+  }
+}
