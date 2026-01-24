db699cc72aee49c61451835c2609022136851b15
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-24 17:41:04 +0100

    provide products cubit to homeView

--- lib/features/home/presentation/views/widgets/home_view.dart
+++ lib/features/home/presentation/views/widgets/home_view.dart
@@ -1,11 +1,19 @@
+import 'package:ecommerce_app/core/cubits/products_cubit/products_cubit.dart';
+import 'package:ecommerce_app/core/repos/products_repo/products_repo.dart';
 import 'package:ecommerce_app/features/home/presentation/views/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../core/services/service_locator.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const HomeViewBody();
+    return BlocProvider(
+      create: (context) => ProductsCubit(getIt<ProductsRepo>()),
+      child: const HomeViewBody(),
+    );
   }
 }
