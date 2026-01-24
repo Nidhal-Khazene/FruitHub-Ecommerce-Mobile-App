30c45ddd8e1d7ab2a449bf97f1b43f0bfd580941
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-24 19:08:08 +0100

    get user: fix if the name was null

--- lib/core/helper/get_user.dart
+++ lib/core/helper/get_user.dart
@@ -5,8 +5,16 @@ import 'package:ecommerce_app/core/services/shared_preferences_singleton.dart';
 import 'package:ecommerce_app/features/auth/data/models/user_model.dart';
 import 'package:ecommerce_app/features/auth/domain/entities/user_entity.dart';
 
-UserEntity getUser() {
-  var jsonData = SharedPreferencesSingleton.getString(kSaveDataSharedPref);
-  UserEntity userEntity = UserModel.fromJson(jsonDecode(jsonData ?? "null"));
-  return userEntity;
+UserEntity? getUser() {
+  final jsonData = SharedPreferencesSingleton.getString(kSaveDataSharedPref);
+
+  if (jsonData == null || jsonData.isEmpty) {
+    return null;
+  }
+
+  final decoded = jsonDecode(jsonData);
+
+  if (decoded == null) return null;
+
+  return UserModel.fromJson(decoded);
 }
