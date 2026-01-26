1cbb3370831f52fbba262a96444df3546c73001c
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-26 17:38:13 +0100

    custom_bottom_navigation_bar: add screens list

--- lib/core/widgets/custom_bottom_navigation_bar.dart
+++ lib/core/widgets/custom_bottom_navigation_bar.dart
@@ -1,14 +1,13 @@
 import 'package:ecommerce_app/features/home/domain/entities/bottom_nav_bar_entity.dart';
+import 'package:ecommerce_app/features/home/presentation/views/home_view.dart';
 import 'package:ecommerce_app/features/home/presentation/views/widgets/bottom_nav_bar_item.dart';
+import 'package:ecommerce_app/features/products/presentation/views/products_view.dart';
 import 'package:flutter/material.dart';
 
 class CustomBottomNavigationBar extends StatefulWidget {
-  const CustomBottomNavigationBar({
-    super.key,
-    required this.bottomNavBarEntity,
-  });
+  const CustomBottomNavigationBar({super.key});
 
-  final BottomNavBarEntity bottomNavBarEntity;
+  static const routeName = "CustomBottomNavigationBar";
 
   @override
   State<CustomBottomNavigationBar> createState() =>
@@ -16,53 +15,63 @@ class CustomBottomNavigationBar extends StatefulWidget {
 }
 
 class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
-  int currentIndex = 0;
+  final BottomNavBarEntity bottomNavBarEntity = BottomNavBarEntity();
+  int selectedIndex = 0;
+  final List<Widget> screens = const [
+    HomeView(),
+    ProductsView(),
+    Placeholder(),
+    Placeholder(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.sizeOf(context).width,
-      height: 70,
-      decoration: const ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30),
-            topRight: Radius.circular(30),
+    return Scaffold(
+      body: screens[selectedIndex],
+      bottomNavigationBar: Container(
+        width: MediaQuery.sizeOf(context).width,
+        height: 70,
+        decoration: const ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
           ),
+          shadows: [
+            BoxShadow(
+              color: Color(0x19000000),
+              blurRadius: 25,
+              offset: Offset(0, -2),
+              spreadRadius: 0,
+            ),
+          ],
         ),
-        shadows: [
-          BoxShadow(
-            color: Color(0x19000000),
-            blurRadius: 25,
-            offset: Offset(0, -2),
-            spreadRadius: 0,
-          ),
-        ],
-      ),
-      child: Row(
-        children: widget.bottomNavBarEntity.getBottomNavBarEntityList
-            .asMap()
-            .entries
-            .map((entry) {
-              var index = entry.key;
-              var e = entry.value;
-              return Expanded(
-                flex: index == currentIndex ? 3 : 2,
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      currentIndex = index;
-                    });
-                  },
-                  child: BottomNavBarItem(
-                    bottomNavBarEntity: e,
-                    isSelected: currentIndex == index,
+        child: Row(
+          children: bottomNavBarEntity.getBottomNavBarEntityList
+              .asMap()
+              .entries
+              .map((entry) {
+                var index = entry.key;
+                var e = entry.value;
+                return Expanded(
+                  flex: index == selectedIndex ? 3 : 2,
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                    child: BottomNavBarItem(
+                      bottomNavBarEntity: e,
+                      isSelected: selectedIndex == index,
+                    ),
                   ),
-                ),
-              );
-            })
-            .toList(),
+                );
+              })
+              .toList(),
+        ),
       ),
     );
   }
--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,7 +1,7 @@
 import 'package:ecommerce_app/constants.dart';
 import 'package:ecommerce_app/core/widgets/custom_bottom_navigation_bar.dart';
 import 'package:ecommerce_app/features/home/domain/entities/bottom_nav_bar_entity.dart';
-import 'package:ecommerce_app/features/home/presentation/views/widgets/home_view.dart';
+import 'package:ecommerce_app/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
 
 class MainView extends StatelessWidget {
