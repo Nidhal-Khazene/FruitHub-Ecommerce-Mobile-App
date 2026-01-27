f8bea8bb6e6cb9eda34f3a5cc6b95c6fb819194b
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-27 20:27:24 +0100

    add our_product_row_item

---
+++ lib/features/products/presentation/views/widgets/our_product_row_item.dart
@@ -0,0 +1,21 @@
+import 'package:ecommerce_app/core/models/our_products_row_model.dart';
+import 'package:ecommerce_app/core/utils/styles.dart';
+import 'package:ecommerce_app/core/widgets/custom_small_oval_container.dart';
+import 'package:flutter/material.dart';
+
+class OurProductRowItem extends StatelessWidget {
+  const OurProductRowItem({super.key, required this.productItem});
+
+  final OurProductsRowModel productItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomSmallOvalContainer(widget: Image.asset(productItem.productImage)),
+        const SizedBox(height: 2),
+        Text(productItem.productName, style: TextStyles.semiBold13),
+      ],
+    );
+  }
+}
---
+++ lib/features/products/presentation/views/widgets/our_products_row.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProductsRow extends StatelessWidget {
+  const ProductsRow({super.key});
+  final List
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
