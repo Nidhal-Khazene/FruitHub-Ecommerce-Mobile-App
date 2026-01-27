6dd06753646ff052ec8ede488c6fc6f5d0c07922
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-27 17:47:12 +0100

    add cusotm small oval container

---
+++ lib/core/widgets/custom_small_oval_container.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CustomSmallOvalContainer extends StatelessWidget {
+  const CustomSmallOvalContainer({super.key, required this.widget});
+  final Widget widget;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 64,
+      height: 64,
+      decoration: const ShapeDecoration(
+        color: Color(0xFFF3F5F7),
+        shape: OvalBorder(),
+      ),
+      child: widget,
+    );
+  }
+}
