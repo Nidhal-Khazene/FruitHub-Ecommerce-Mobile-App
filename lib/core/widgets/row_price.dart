64e9aceba87ff14feb9201a99cddfb36080ffb0c
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-29 10:10:09 +0100

    add row price

---
+++ lib/core/widgets/row_price.dart
@@ -0,0 +1,21 @@
+import 'package:ecommerce_app/core/widgets/price_container.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/styles.dart';
+
+class RowPrice extends StatelessWidget {
+  const RowPrice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Expanded(child: PriceContainer()),
+        SizedBox(width: 14),
+        Text("الي", style: TextStyles.bold13),
+        SizedBox(width: 14),
+        Expanded(child: PriceContainer()),
+      ],
+    );
+  }
+}
