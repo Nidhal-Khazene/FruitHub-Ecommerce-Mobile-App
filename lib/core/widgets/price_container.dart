4b01a0298ec4a639b1ae5b78e6c18a8ef0174606
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-29 10:09:42 +0100

    add price container

---
+++ lib/core/widgets/price_container.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../utils/colors.dart';
+import '../utils/styles.dart';
+import 'custom_text_form_field.dart';
+
+class PriceContainer extends StatelessWidget {
+  const PriceContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextFormField(
+      hintTextAlign: TextAlign.center,
+      hintText: "0",
+      hintStyle: TextStyles.regular13.copyWith(
+        color: ColorData.kFontSecondaryColor,
+      ),
+      borderRadius: BorderRadius.circular(8),
+      fillColor: Colors.white,
+      textInputType: TextInputType.number,
+    );
+  }
+}
