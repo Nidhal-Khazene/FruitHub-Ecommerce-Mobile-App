cec08b7696015afa693be057d69a497cf35affd4
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-29 10:12:28 +0100

    products search bar: add price row, row container...

--- lib/features/products/presentation/views/widgets/products_search_bar.dart
+++ lib/features/products/presentation/views/widgets/products_search_bar.dart
@@ -1,8 +1,11 @@
+import 'package:ecommerce_app/core/utils/styles.dart';
+import 'package:ecommerce_app/core/widgets/bottom_sheet_line_header.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 import '../../../../../core/utils/colors.dart';
 import '../../../../../core/widgets/custom_search_bar.dart';
+import '../../../../../core/widgets/row_price.dart';
 
 class ProductsSearchBar extends StatelessWidget {
   const ProductsSearchBar({super.key});
@@ -10,6 +13,59 @@ class ProductsSearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomSearchBar(
+      onTap: () {
+        showModalBottomSheet(
+          isScrollControlled: true,
+          backgroundColor: Colors.white,
+          context: context,
+          builder: (context) {
+            return const Padding(
+              padding: EdgeInsets.only(
+                top: 32,
+                bottom: 32,
+                left: 13,
+                right: 22,
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: BottomSheetLineHeader(
+                        lineColor: Color(0xFF131F46),
+                        width: 60,
+                        height: 3,
+                      ),
+                    ),
+                    SizedBox(height: 7),
+                    Text("تصنيف حسب :", style: TextStyles.bold19),
+                    SizedBox(height: 11),
+                    Padding(
+                      padding: EdgeInsets.only(right: 8),
+                      child: Row(
+                        children: [
+                          Icon(Iconsax.tag_copy, color: Colors.black),
+                          SizedBox(width: 8),
+                          Text("السعر :", style: TextStyles.bold13),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: RowPrice(),
+                    ),
+                    SizedBox(height: 16),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
       borderWidth: 0,
       fillColor: Colors.transparent,
       hintText: "ابحث عن.......",
