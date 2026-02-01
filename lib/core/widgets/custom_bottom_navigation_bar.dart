9353ff51a27274db919306207959cc3d03fc54fd
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-01 13:44:17 +0100

    provide cart cubit to custom bottom nav bar so to all the navigation widgets

--- lib/core/widgets/custom_bottom_navigation_bar.dart
+++ lib/core/widgets/custom_bottom_navigation_bar.dart
@@ -1,9 +1,11 @@
 import 'package:ecommerce_app/core/entities/bottom_nav_bar_entity.dart';
 import 'package:ecommerce_app/core/widgets/bottom_nav_bar_item.dart';
+import 'package:ecommerce_app/features/cart/presentation/manager/cubits/cart_cubit/cart_cubit.dart';
 import 'package:ecommerce_app/features/cart/presentation/views/cart_view.dart';
 import 'package:ecommerce_app/features/home/presentation/views/home_view.dart';
 import 'package:ecommerce_app/features/products/presentation/views/products_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CustomBottomNavigationBar extends StatefulWidget {
   const CustomBottomNavigationBar({super.key});
@@ -21,54 +23,62 @@ class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: selectedIndex,
-        children: const [HomeView(), ProductsView(), CartView(), Placeholder()],
-      ),
-      bottomNavigationBar: Container(
-        width: MediaQuery.sizeOf(context).width,
-        height: 70,
-        decoration: const ShapeDecoration(
-          color: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
-            ),
-          ),
-          shadows: [
-            BoxShadow(
-              color: Color(0x19000000),
-              blurRadius: 25,
-              offset: Offset(0, -2),
-              spreadRadius: 0,
-            ),
+    return BlocProvider(
+      create: (context) => CartCubit(),
+      child: Scaffold(
+        body: IndexedStack(
+          index: selectedIndex,
+          children: const [
+            HomeView(),
+            ProductsView(),
+            CartView(),
+            Placeholder(),
           ],
         ),
-        child: Row(
-          children: bottomNavBarEntity.getBottomNavBarEntityList
-              .asMap()
-              .entries
-              .map((entry) {
-                var index = entry.key;
-                var e = entry.value;
-                return Expanded(
-                  flex: index == selectedIndex ? 3 : 2,
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedIndex = index;
-                      });
-                    },
-                    child: BottomNavBarItem(
-                      bottomNavBarEntity: e,
-                      isSelected: selectedIndex == index,
+        bottomNavigationBar: Container(
+          width: MediaQuery.sizeOf(context).width,
+          height: 70,
+          decoration: const ShapeDecoration(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+            ),
+            shadows: [
+              BoxShadow(
+                color: Color(0x19000000),
+                blurRadius: 25,
+                offset: Offset(0, -2),
+                spreadRadius: 0,
+              ),
+            ],
+          ),
+          child: Row(
+            children: bottomNavBarEntity.getBottomNavBarEntityList
+                .asMap()
+                .entries
+                .map((entry) {
+                  var index = entry.key;
+                  var e = entry.value;
+                  return Expanded(
+                    flex: index == selectedIndex ? 3 : 2,
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                      child: BottomNavBarItem(
+                        bottomNavBarEntity: e,
+                        isSelected: selectedIndex == index,
+                      ),
                     ),
-                  ),
-                );
-              })
-              .toList(),
+                  );
+                })
+                .toList(),
+          ),
         ),
       ),
     );
