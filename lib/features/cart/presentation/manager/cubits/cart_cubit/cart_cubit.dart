82c47d5f8f680e9a62a98371e6de1cb1886b1b6a
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-01 09:22:21 +0100

    add cart entity and cart cubit and cart state

--- lib/features/cart/presentation/manager/cubits/cart_cubit/cart_cubit.dart
+++ lib/features/cart/presentation/manager/cubits/cart_cubit/cart_cubit.dart
@@ -1,8 +1,15 @@
 import 'package:bloc/bloc.dart';
+import 'package:ecommerce_app/features/cart/domain/entities/cart_entity.dart';
+import 'package:ecommerce_app/features/cart/domain/entities/cart_item_entity.dart';
 import 'package:meta/meta.dart';
 
 part 'cart_state.dart';
 
 class CartCubit extends Cubit<CartState> {
   CartCubit() : super(CartInitial());
+  CartEntity cartEntity = CartEntity(cartItems: []);
+  void addCartItem(CartItemEntity cartItemEntity) {
+    cartEntity.addCartItem(cartItemEntity);
+    emit(CartAdded());
+  }
 }
--- lib/features/cart/presentation/manager/cubits/cart_cubit/cart_state.dart
+++ lib/features/cart/presentation/manager/cubits/cart_cubit/cart_state.dart
@@ -4,3 +4,7 @@ part of 'cart_cubit.dart';
 sealed class CartState {}
 
 final class CartInitial extends CartState {}
+
+final class CartAdded extends CartState {}
+
+final class CartRemoved extends CartState {}
