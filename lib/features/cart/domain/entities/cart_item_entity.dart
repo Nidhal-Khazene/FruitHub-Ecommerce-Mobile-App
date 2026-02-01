8f5801089b95bda8c7585942289fe4a848373f5e
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-01 08:25:01 +0100

    add cart item entity

--- lib/features/cart/domain/entities/cart_item_entity.dart
+++ lib/features/cart/domain/entities/cart_item_entity.dart
@@ -1,1 +1,8 @@
-class CartItemEntity {}
+import '../../../../core/entities/product_entity.dart';
+
+class CartItemEntity {
+  final ProductEntity productEntity;
+  int count;
+
+  CartItemEntity({required this.productEntity, this.count = 0});
+}
