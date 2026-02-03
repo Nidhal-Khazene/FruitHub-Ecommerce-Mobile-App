e6dd40bf02cbffca03156ee610a75963d08bcaae
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-03 17:36:05 +0100

    add active step widget

---
+++ lib/features/checkout/presentation/views/widgets/active_step.dart
@@ -0,0 +1,22 @@
+import 'package:ecommerce_app/core/utils/colors.dart';
+import 'package:ecommerce_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class ActiveStep extends StatelessWidget {
+  const ActiveStep({super.key, required this.textContent});
+  final String textContent;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CircleAvatar(
+          radius: 11.5,
+          backgroundColor: ColorData.kPrimaryColor,
+          child: const Icon(Icons.check, color: Colors.white, size: 15),
+        ),
+        const SizedBox(width: 6),
+        Text(textContent, style: AppStyles.bold13),
+      ],
+    );
+  }
+}
