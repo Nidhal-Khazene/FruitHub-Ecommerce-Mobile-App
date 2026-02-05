ee076f54869153494fb4d1df2b1001ff651057b8
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-05 11:56:07 +0100

    payment success view: add navigations to button

--- lib/features/checkout/presentation/views/widgets/payment_success_view.dart
+++ lib/features/checkout/presentation/views/widgets/payment_success_view.dart
@@ -1,5 +1,10 @@
 import 'package:ecommerce_app/constants.dart';
+import 'package:ecommerce_app/core/utils/assets.dart';
+import 'package:ecommerce_app/core/utils/colors.dart';
 import 'package:ecommerce_app/core/utils/styles.dart';
+import 'package:ecommerce_app/core/widgets/custom_bottom_navigation_bar.dart';
+import 'package:ecommerce_app/core/widgets/custom_button.dart';
+import 'package:ecommerce_app/features/checkout/presentation/views/widgets/track_order_view.dart';
 import 'package:flutter/material.dart';
 
 class PaymentSuccessView extends StatelessWidget {
@@ -11,6 +16,10 @@ class PaymentSuccessView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
         title: const Text(
           'الدفع',
           textAlign: TextAlign.center,
@@ -20,7 +29,52 @@ class PaymentSuccessView extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: kPrimaryScreenPadding,
-          child: Column(children: [SizedBox(height: 67)]),
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(height: 67),
+                Image.asset(
+                  Assets.assetsImagesSuccessImage,
+                  width: 154,
+                  height: 107,
+                ),
+                const SizedBox(height: 33),
+                Column(
+                  children: [
+                    const Text('تم بنجاح !', style: AppStyles.bold16),
+                    const SizedBox(height: 9),
+                    Text(
+                      'رقم الطلب : 1245789663#',
+                      style: AppStyles.regular13.copyWith(
+                        color: ColorData.kFontMediumColor,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 243),
+                CustomButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, TrackOrderView.routeName);
+                  },
+                  text: "تتبع الطلب",
+                ),
+                CustomButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(
+                      context,
+                      CustomBottomNavigationBar.routeName,
+                    );
+                  },
+                  text: "الرئيسية",
+                  textStyle: AppStyles.bold16.copyWith(
+                    color: ColorData.kPrimaryColor,
+                    decoration: TextDecoration.underline,
+                  ),
+                  backgroundColor: Colors.transparent,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
