67d88d29db1cb5e68b0642aa58425d3eeb1f429b
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-05 08:44:17 +0100

    fix checkout button: update page number

--- lib/features/checkout/presentation/views/widgets/checkout_button.dart
+++ lib/features/checkout/presentation/views/widgets/checkout_button.dart
@@ -3,22 +3,53 @@ import 'package:flutter/material.dart';
 import '../../../../../core/utils/styles.dart';
 import '../../../../../core/widgets/custom_button.dart';
 
-class CheckoutButton extends StatelessWidget {
+class CheckoutButton extends StatefulWidget {
   const CheckoutButton({super.key, required PageController pageController})
     : _pageController = pageController;
 
   final PageController _pageController;
 
+  static const List<String> titles = [
+    "التالي",
+    "التالي",
+    "تأكيد & استمرار",
+    "تأكيد الطلب",
+  ];
+
+  @override
+  State<CheckoutButton> createState() => _CheckoutButtonState();
+}
+
+class _CheckoutButtonState extends State<CheckoutButton> {
+  int pageNumber = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Listen to page changes
+    widget._pageController.addListener(() {
+      final int newPage = widget._pageController.page?.round() ?? 0;
+
+      if (newPage != pageNumber) {
+        setState(() {
+          pageNumber = newPage;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return CustomButton(
       onPressed: () {
-        _pageController.nextPage(
+        widget._pageController.nextPage(
           duration: const Duration(milliseconds: 300),
           curve: Curves.easeInOut,
         );
       },
-      text: "التالي",
+      text: CheckoutButton
+          .titles[pageNumber.clamp(0, CheckoutButton.titles.length - 1)],
       textStyle: AppStyles.bold16.copyWith(color: Colors.white),
     );
   }
