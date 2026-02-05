1b32a0f5eeaf1cd4fd33625d330a895afa04750f
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-05 09:00:18 +0100

    add payment review ticket

---
+++ lib/features/checkout/presentation/views/widgets/payment_review_ticket.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/colors.dart';
+import '../../../../../core/utils/styles.dart';
+
+class PaymentReviewTicket extends StatelessWidget {
+  const PaymentReviewTicket({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 128,
+      padding: const EdgeInsets.symmetric(horizontal: 9, vertical: 5),
+      decoration: ShapeDecoration(
+        color: const Color(0x7FF2F3F3),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Text(
+                'المجموع الفرعي :',
+                style: AppStyles.regular13.copyWith(
+                  color: ColorData.kFontSecondaryColor,
+                ),
+              ),
+              const Expanded(child: SizedBox()),
+              const Text(
+                '150 جنيه',
+                textAlign: TextAlign.right,
+                style: AppStyles.semiBold16,
+              ),
+            ],
+          ),
+          const SizedBox(height: 8),
+          Row(
+            children: [
+              Text(
+                'التوصيل  :',
+                style: AppStyles.regular13.copyWith(
+                  color: ColorData.kFontSecondaryColor,
+                ),
+              ),
+              const Expanded(child: SizedBox()),
+              Text(
+                '30جنية',
+                textAlign: TextAlign.right,
+                style: AppStyles.semiBold13.copyWith(
+                  color: ColorData.kFontSecondaryColor,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 8),
+          Container(
+            width: 274,
+            decoration: const ShapeDecoration(
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                  width: 0.50,
+                  strokeAlign: BorderSide.strokeAlignCenter,
+                  color: Color(0xFFCACECE) /* Grayscale-200 */,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          const Row(
+            children: [
+              Text('الكلي', style: AppStyles.bold16),
+              Expanded(child: SizedBox()),
+              Text(
+                '180 جنيه',
+                textAlign: TextAlign.right,
+                style: AppStyles.bold16,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
