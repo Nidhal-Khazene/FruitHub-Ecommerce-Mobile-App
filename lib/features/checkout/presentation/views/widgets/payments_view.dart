26e486e9fe665155e25876cb4c9e27380b45dc20
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-05 08:14:39 +0100

    payments view ui done

--- lib/features/checkout/presentation/views/widgets/payments_view.dart
+++ lib/features/checkout/presentation/views/widgets/payments_view.dart
@@ -1,7 +1,10 @@
 import 'package:ecommerce_app/core/utils/colors.dart';
 import 'package:ecommerce_app/core/utils/styles.dart';
 import 'package:ecommerce_app/features/checkout/presentation/views/widgets/payments_methods.dart';
+import 'package:ecommerce_app/features/checkout/presentation/views/widgets/virtual_card_payments_check_box.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../../core/widgets/custom_text_form_field.dart';
 
 class PaymentsView extends StatelessWidget {
   const PaymentsView({super.key});
@@ -23,6 +26,30 @@ class PaymentsView extends StatelessWidget {
         const SizedBox(height: 13),
         const PaymentsMethods(),
         const SizedBox(height: 16),
+        const CustomTextFormField(hintText: "اسم حامل البطاقه"),
+        const SizedBox(height: 8),
+        const CustomTextFormField(hintText: "رقم البطاقة"),
+        const SizedBox(height: 8),
+        const Row(
+          children: [
+            Expanded(child: CustomTextFormField(hintText: "تاريخ الصلاحيه")),
+            SizedBox(width: 16),
+            Expanded(child: CustomTextFormField(hintText: "CVV")),
+          ],
+        ),
+        const SizedBox(height: 17),
+        Row(
+          children: [
+            const VirtualCardPaymentsCheckBox(),
+            const SizedBox(width: 16),
+            Text(
+              "جعل البطاقة افتراضية",
+              style: AppStyles.semiBold13.copyWith(
+                color: ColorData.kFontSecondaryColor,
+              ),
+            ),
+          ],
+        ),
       ],
     );
   }
