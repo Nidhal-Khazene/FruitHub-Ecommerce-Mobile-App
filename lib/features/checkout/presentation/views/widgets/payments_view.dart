9a7bcd04b48512b3c578385d17fadf85d732e5e9
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-09 18:19:06 +0100

    payments view: make the view scrollable

--- lib/features/checkout/presentation/views/widgets/payments_view.dart
+++ lib/features/checkout/presentation/views/widgets/payments_view.dart
@@ -11,46 +11,48 @@ class PaymentsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(height: 24),
-        const Text("أختار طريقه الدفع المناسبه :", style: AppStyles.bold13),
-        const SizedBox(height: 13),
-        Text(
-          "من فضلك اختر طريقه الدفع المناسبه لك.",
-          style: AppStyles.regular13.copyWith(
-            color: ColorData.kFontSecondaryColor,
-          ),
-        ),
-        const SizedBox(height: 13),
-        const PaymentsMethods(),
-        const SizedBox(height: 16),
-        const CustomTextFormField(hintText: "اسم حامل البطاقه"),
-        const SizedBox(height: 8),
-        const CustomTextFormField(hintText: "رقم البطاقة"),
-        const SizedBox(height: 8),
-        const Row(
-          children: [
-            Expanded(child: CustomTextFormField(hintText: "تاريخ الصلاحيه")),
-            SizedBox(width: 16),
-            Expanded(child: CustomTextFormField(hintText: "CVV")),
-          ],
-        ),
-        const SizedBox(height: 17),
-        Row(
-          children: [
-            const VirtualCardPaymentsCheckBox(),
-            const SizedBox(width: 16),
-            Text(
-              "جعل البطاقة افتراضية",
-              style: AppStyles.semiBold13.copyWith(
-                color: ColorData.kFontSecondaryColor,
-              ),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 24),
+          const Text("أختار طريقه الدفع المناسبه :", style: AppStyles.bold13),
+          const SizedBox(height: 13),
+          Text(
+            "من فضلك اختر طريقه الدفع المناسبه لك.",
+            style: AppStyles.regular13.copyWith(
+              color: ColorData.kFontSecondaryColor,
             ),
-          ],
-        ),
-      ],
+          ),
+          const SizedBox(height: 13),
+          const PaymentsMethods(),
+          const SizedBox(height: 16),
+          const CustomTextFormField(hintText: "اسم حامل البطاقه"),
+          const SizedBox(height: 8),
+          const CustomTextFormField(hintText: "رقم البطاقة"),
+          const SizedBox(height: 8),
+          const Row(
+            children: [
+              Expanded(child: CustomTextFormField(hintText: "تاريخ الصلاحيه")),
+              SizedBox(width: 16),
+              Expanded(child: CustomTextFormField(hintText: "CVV")),
+            ],
+          ),
+          const SizedBox(height: 17),
+          Row(
+            children: [
+              const VirtualCardPaymentsCheckBox(),
+              const SizedBox(width: 16),
+              Text(
+                "جعل البطاقة افتراضية",
+                style: AppStyles.semiBold13.copyWith(
+                  color: ColorData.kFontSecondaryColor,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
