7e1265d05462a36c66c71ea89c1afb2e5d0ebbf1
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-10 10:16:23 +0100

    add PaymentCardEntity instance to OrderEntity

--- lib/features/checkout/presentation/views/checkout_view.dart
+++ lib/features/checkout/presentation/views/checkout_view.dart
@@ -2,6 +2,7 @@ import 'package:ecommerce_app/constants.dart';
 import 'package:ecommerce_app/core/widgets/custom_app_bar.dart';
 import 'package:ecommerce_app/features/cart/domain/entities/cart_entity.dart';
 import 'package:ecommerce_app/features/checkout/domain/entities/order_entity.dart';
+import 'package:ecommerce_app/features/checkout/domain/entities/payment_card_entity.dart';
 import 'package:ecommerce_app/features/checkout/domain/entities/shipping_address_entity.dart';
 import 'package:ecommerce_app/features/checkout/presentation/views/widgets/checkout_view_body.dart';
 import 'package:flutter/material.dart';
@@ -20,9 +21,11 @@ class CheckoutView extends StatefulWidget {
 
 class _CheckoutViewState extends State<CheckoutView> {
   late OrderEntity orderEntity;
+
   @override
   void initState() {
     orderEntity = OrderEntity(
+      paymentCardEntity: PaymentCardEntity(),
       cartEntity: widget.cartEntity,
       shippingAddressEntity: ShippingAddressEntity(),
     );
