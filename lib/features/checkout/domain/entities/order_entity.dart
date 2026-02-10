778319482e61d678dfa9c27052b227d2d7701fb1
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-10 10:14:47 +0100

    add PaymentCardEntity to OrderEntity and make its attribute nullable

--- lib/features/checkout/domain/entities/order_entity.dart
+++ lib/features/checkout/domain/entities/order_entity.dart
@@ -1,14 +1,17 @@
 import 'package:ecommerce_app/features/cart/domain/entities/cart_entity.dart';
+import 'package:ecommerce_app/features/checkout/domain/entities/payment_card_entity.dart';
 import 'package:ecommerce_app/features/checkout/domain/entities/shipping_address_entity.dart';
 
 class OrderEntity {
   final CartEntity cartEntity;
+  PaymentCardEntity paymentCardEntity;
   bool? payWithCash;
   ShippingAddressEntity shippingAddressEntity;
 
   OrderEntity({
     required this.cartEntity,
     this.payWithCash,
+    required this.paymentCardEntity,
     required this.shippingAddressEntity,
   });
 }
--- lib/features/checkout/domain/entities/payment_card_entity.dart
+++ lib/features/checkout/domain/entities/payment_card_entity.dart
@@ -1,13 +1,13 @@
 class PaymentCardEntity {
-  final String name;
-  final String cardNumber;
-  final String expirationCard;
-  final String cvv;
+  String? name;
+  String? cardNumber;
+  String? expirationCard;
+  String? cvv;
 
   PaymentCardEntity({
-    required this.name,
-    required this.cardNumber,
-    required this.expirationCard,
-    required this.cvv,
+    this.name,
+    this.cardNumber,
+    this.expirationCard,
+    this.cvv,
   });
 }
