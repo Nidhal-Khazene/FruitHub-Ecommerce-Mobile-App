09b28ec697a6a41fd73b49104c7abdf5bfb284c2
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-14 10:47:09 +0100

    add order cubit added

--- lib/features/checkout/presentation/manager/add_order_cubit/add_order_cubit.dart
+++ lib/features/checkout/presentation/manager/add_order_cubit/add_order_cubit.dart
@@ -1,8 +1,20 @@
 import 'package:bloc/bloc.dart';
+import 'package:ecommerce_app/core/repos/orders_repo/order_repo.dart';
+import 'package:ecommerce_app/features/checkout/domain/entities/order_entity.dart';
 import 'package:meta/meta.dart';
 
 part 'add_order_state.dart';
 
 class AddOrderCubit extends Cubit<AddOrderState> {
-  AddOrderCubit() : super(AddOrderInitial());
+  AddOrderCubit({required this.orderRepo}) : super(AddOrderInitial());
+  final OrderRepo orderRepo;
+
+  Future<void> addOrder(OrderEntity orderEntity) async {
+    emit(AddOrderLoading());
+    var result = await orderRepo.addOrder(orderEntity);
+    result.fold(
+      (failure) => emit(AddOrderFailure(errorMessage: failure.message)),
+      (right) => emit(AddOrderSuccess()),
+    );
+  }
 }
