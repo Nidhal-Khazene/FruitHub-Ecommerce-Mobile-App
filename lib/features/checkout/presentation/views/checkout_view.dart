3af77f5831cde151808851a00a67bbdd8a0bede6
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-14 10:52:19 +0100

    checkout view: provide add order cubit

--- lib/features/checkout/presentation/views/checkout_view.dart
+++ lib/features/checkout/presentation/views/checkout_view.dart
@@ -1,13 +1,18 @@
 import 'package:ecommerce_app/constants.dart';
 import 'package:ecommerce_app/core/helper/get_user.dart';
+import 'package:ecommerce_app/core/repos/orders_repo/order_repo.dart';
 import 'package:ecommerce_app/core/widgets/custom_app_bar.dart';
 import 'package:ecommerce_app/features/cart/domain/entities/cart_entity.dart';
 import 'package:ecommerce_app/features/checkout/domain/entities/order_entity.dart';
 import 'package:ecommerce_app/features/checkout/domain/entities/payment_card_entity.dart';
 import 'package:ecommerce_app/features/checkout/domain/entities/shipping_address_entity.dart';
+import 'package:ecommerce_app/features/checkout/presentation/manager/add_order_cubit/add_order_cubit.dart';
 import 'package:ecommerce_app/features/checkout/presentation/views/widgets/checkout_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
+
+import '../../../../core/services/service_locator.dart';
 
 class CheckoutView extends StatefulWidget {
   const CheckoutView({super.key, required this.cartEntity});
@@ -36,21 +41,24 @@ class _CheckoutViewState extends State<CheckoutView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildCustomAppBar(context, title: "الشحن"),
-      body: SafeArea(
-        child: Padding(
-          padding: kPrimaryScreenPadding,
-          child: MultiProvider(
-            providers: [
-              Provider<OrderEntity>(create: (_) => orderEntity),
-              Provider<AddressStepScope>(create: (_) => AddressStepScope()),
-              Provider<PaymentsStepScope>(create: (_) => PaymentsStepScope()),
-              Provider<PaymentsReviewStepScope>(
-                create: (_) => PaymentsReviewStepScope(),
-              ),
-            ],
-            child: const CheckoutViewBody(),
+    return BlocProvider(
+      create: (context) => AddOrderCubit(orderRepo: getIt.get<OrderRepo>()),
+      child: Scaffold(
+        appBar: buildCustomAppBar(context, title: "الشحن"),
+        body: SafeArea(
+          child: Padding(
+            padding: kPrimaryScreenPadding,
+            child: MultiProvider(
+              providers: [
+                Provider<OrderEntity>(create: (_) => orderEntity),
+                Provider<AddressStepScope>(create: (_) => AddressStepScope()),
+                Provider<PaymentsStepScope>(create: (_) => PaymentsStepScope()),
+                Provider<PaymentsReviewStepScope>(
+                  create: (_) => PaymentsReviewStepScope(),
+                ),
+              ],
+              child: const CheckoutViewBody(),
+            ),
           ),
         ),
       ),
