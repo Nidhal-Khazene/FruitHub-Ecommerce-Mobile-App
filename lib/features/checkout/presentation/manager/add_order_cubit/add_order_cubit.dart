205b6cddbbed663eb20d5a16bce70ce944557e84
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-14 10:41:48 +0100

    add order state added

---
+++ lib/features/checkout/presentation/manager/add_order_cubit/add_order_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'add_order_state.dart';
+
+class AddOrderCubit extends Cubit<AddOrderState> {
+  AddOrderCubit() : super(AddOrderInitial());
+}
