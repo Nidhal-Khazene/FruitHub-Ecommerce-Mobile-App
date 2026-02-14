2a44f5c5c7e0b2ef6bdfb7e60e2a42a6046cdc6d
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-14 08:59:05 +0100

    order model: add orderProducts and paymentMethod

--- lib/features/checkout/data/models/order_model.dart
+++ lib/features/checkout/data/models/order_model.dart
@@ -1,11 +1,16 @@
+import 'package:ecommerce_app/features/checkout/data/models/product_order_model.dart';
 import 'package:ecommerce_app/features/checkout/data/models/shipping_address_model.dart';
 
 class OrderModel {
   final double totalPrice;
   final String uID;
   final ShippingAddressModel shippingAddressModel;
+  final List<ProductOrderModel> orderProducts;
+  final String paymentMethod;
 
   OrderModel({
+    required this.orderProducts,
+    required this.paymentMethod,
     required this.totalPrice,
     required this.uID,
     required this.shippingAddressModel,
