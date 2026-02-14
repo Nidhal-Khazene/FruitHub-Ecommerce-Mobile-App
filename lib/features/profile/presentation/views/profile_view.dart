8b099e3f5ffa337364cb7c396e3027411527c12b
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-14 11:26:53 +0100

    add profile view structure

---
+++ lib/features/profile/presentation/views/profile_view.dart
@@ -0,0 +1,19 @@
+import 'package:ecommerce_app/constants.dart';
+import 'package:ecommerce_app/features/profile/presentation/views/widgets/profile_view_body.dart';
+import 'package:flutter/material.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: kPrimaryScreenPadding,
+          child: ProfileViewBody(),
+        ),
+      ),
+    );
+  }
+}
