4d2b878bbbc6c21c5953e6b1c40960abb6867c05
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-14 08:51:50 +0100

    add product order model

---
+++ lib/features/checkout/data/models/product_order_model.dart
@@ -0,0 +1,27 @@
+import 'package:ecommerce_app/features/cart/domain/entities/cart_item_entity.dart';
+
+class ProductOrderModel {
+  final String productCode;
+  final String productName;
+  final num productPrice;
+  final String imageUrl;
+  final int quantity;
+
+  ProductOrderModel({
+    required this.productCode,
+    required this.productName,
+    required this.productPrice,
+    required this.imageUrl,
+    required this.quantity,
+  });
+
+  factory ProductOrderModel.fromEntity(CartItemEntity cartItemEntity) {
+    return ProductOrderModel(
+      productCode: cartItemEntity.productEntity.productCode,
+      productName: cartItemEntity.productEntity.productName,
+      productPrice: cartItemEntity.productEntity.productPrice,
+      imageUrl: cartItemEntity.productEntity.urlImage!,
+      quantity: cartItemEntity.quantity,
+    );
+  }
+}
