51eb73ae5dbd9b420184d18cd2310989f79551fc
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-14 10:11:05 +0100

    add order repo

---
+++ lib/core/repos/orders_repo/order_repo.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:ecommerce_app/core/errors/failures.dart';
+import 'package:ecommerce_app/features/checkout/domain/entities/order_entity.dart';
+
+abstract class OrderRepo {
+  Future<Either<Failure, void>> addOrder(OrderEntity orderEntity);
+}
