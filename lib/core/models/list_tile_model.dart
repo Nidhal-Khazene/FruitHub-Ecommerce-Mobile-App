cfbf6bc116ec978c5e6480b5cf78ce72aa7723b2
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-15 11:53:15 +0100

    rename text to title

--- lib/core/models/list_tile_model.dart
+++ lib/core/models/list_tile_model.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class ListTileModel {
-  final String text;
+  final String title;
   final String subTitle;
   final Widget trailing;
   final Widget leading;
 
   ListTileModel({
-    required this.text,
+    required this.title,
     required this.subTitle,
     required this.trailing,
     required this.leading,
