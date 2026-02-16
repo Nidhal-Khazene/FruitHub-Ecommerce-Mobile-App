eba2183e8d0fac169f77fcb8aedbf236db212747
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-16 17:20:43 +0100

    personal profile view done

--- lib/features/profile/presentation/views/widgets/personal_profile_view.dart
+++ lib/features/profile/presentation/views/widgets/personal_profile_view.dart
@@ -1,6 +1,11 @@
 import 'package:ecommerce_app/constants.dart';
 import 'package:ecommerce_app/core/widgets/custom_app_bar.dart';
+import 'package:ecommerce_app/core/widgets/custom_button.dart';
+import 'package:ecommerce_app/core/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+
+import '../../../../../core/utils/styles.dart';
 
 class PersonalProfileView extends StatelessWidget {
   const PersonalProfileView({super.key});
@@ -11,8 +16,75 @@ class PersonalProfileView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildCustomAppBar(context, title: "الملف الشخصي"),
-      body: const SafeArea(
-        child: Padding(padding: kPrimaryScreenPadding, child: Column()),
+      body: SafeArea(
+        child: Padding(
+          padding: kPrimaryScreenPadding,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 16),
+                Text(
+                  'المعلومات الشخصيه',
+                  style: AppStyles.semiBold13.copyWith(
+                    color: const Color(0xFF0C0D0D),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                const CustomTextFormField(
+                  hintText: "أحمد ياسر",
+                  suffixIcon: Icon(
+                    Iconsax.edit_2_copy,
+                    color: Color(0xFF949D9E),
+                    size: 16,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                const CustomTextFormField(
+                  hintText: "mail@mail.com",
+                  suffixIcon: Icon(
+                    Iconsax.edit_2_copy,
+                    color: Color(0xFF949D9E),
+                    size: 16,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  'تغيير كلمة المرور',
+                  style: AppStyles.semiBold13.copyWith(
+                    color: const Color(0xFF0C0D0D),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                const CustomTextFormField(
+                  hintText: "كلمة المرور الحالي",
+                  suffixIcon: Padding(
+                    padding: EdgeInsets.only(left: 33),
+                    child: Icon(Iconsax.eye, color: Color(0xffC9CECF)),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                const CustomTextFormField(
+                  hintText: "كلمة المرور الجديده",
+                  suffixIcon: Padding(
+                    padding: EdgeInsets.only(left: 33),
+                    child: Icon(Iconsax.eye, color: Color(0xffC9CECF)),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                const CustomTextFormField(
+                  hintText: "تأكيد كلمة المرور الجديده",
+                  suffixIcon: Padding(
+                    padding: EdgeInsets.only(left: 33),
+                    child: Icon(Iconsax.eye, color: Color(0xffC9CECF)),
+                  ),
+                ),
+                const SizedBox(height: 74),
+                const CustomButton(text: "حفظ التغييرات"),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
