d4a13f8b85ce638f6abe3cb6666749a0a6e88bd6
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-16 11:33:47 +0100

    profile body section done

--- lib/features/profile/presentation/views/widgets/profile_body_section.dart
+++ lib/features/profile/presentation/views/widgets/profile_body_section.dart
@@ -1,62 +1,92 @@
 import 'package:ecommerce_app/core/models/list_tile_model.dart';
 import 'package:ecommerce_app/core/utils/colors.dart';
 import 'package:ecommerce_app/core/utils/styles.dart';
+import 'package:ecommerce_app/core/widgets/toggle_container_switch.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class ProfileBodySection extends StatelessWidget {
   const ProfileBodySection({super.key});
+
   static final List<ListTileModel> listTileItems = [
     ListTileModel(
       title: "الملف الشخصي",
-      trailing: Icon(
+      leading: Icon(
         Iconsax.user_copy,
         size: 20,
         color: ColorData.kPrimaryColor,
       ),
-      leading: const Icon(Icons.arrow_forward_ios, size: 20),
+      trailing: const Icon(Icons.arrow_forward_ios, size: 20),
     ),
     ListTileModel(
       title: "طلباتي",
-      trailing: Icon(
+      leading: Icon(
         Iconsax.box_1_copy,
         size: 20,
         color: ColorData.kPrimaryColor,
       ),
-      leading: const Icon(Icons.arrow_forward_ios, size: 20),
+      trailing: const Icon(Icons.arrow_forward_ios, size: 20),
     ),
     ListTileModel(
-      title: "طلباتي",
-      trailing: Icon(
-        Iconsax.box_1_copy,
+      title: "المدفوعات",
+      leading: Icon(
+        Iconsax.empty_wallet_copy,
         size: 20,
         color: ColorData.kPrimaryColor,
       ),
-      leading: const Icon(Icons.arrow_forward_ios, size: 20),
+      trailing: const Icon(Icons.arrow_forward_ios, size: 20),
     ),
     ListTileModel(
-      title: "طلباتي",
-      trailing: Icon(
-        Iconsax.box_1_copy,
+      title: "المفضلة",
+      leading: Icon(
+        Iconsax.heart_copy,
         size: 20,
         color: ColorData.kPrimaryColor,
       ),
-      leading: const Icon(Icons.arrow_forward_ios, size: 20),
+      trailing: const Icon(Icons.arrow_forward_ios, size: 20),
     ),
     ListTileModel(
-      title: "طلباتي",
-      trailing: Icon(
-        Iconsax.box_1_copy,
+      title: "الاشعارات",
+      leading: Icon(
+        Iconsax.notification_copy,
         size: 20,
         color: ColorData.kPrimaryColor,
       ),
-      leading: const Icon(Icons.arrow_forward_ios, size: 20),
+      trailing: const ToggleContainerSwitch(),
+    ),
+    ListTileModel(
+      title: "اللغة",
+      leading: Icon(
+        Iconsax.global_copy,
+        size: 20,
+        color: ColorData.kPrimaryColor,
+      ),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'العربية',
+            style: AppStyles.regular13.copyWith(color: const Color(0xFF0C0D0D)),
+          ),
+          const SizedBox(width: 4),
+          const Icon(Icons.arrow_forward_ios, size: 20),
+        ],
+      ),
+    ),
+    ListTileModel(
+      title: "الوضع",
+      leading: Icon(
+        Iconsax.magicpen_copy,
+        size: 20,
+        color: ColorData.kPrimaryColor,
+      ),
+      trailing: const ToggleContainerSwitch(),
     ),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
+    return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -67,23 +97,44 @@ class ProfileBodySection extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 16),
-          Expanded(
-            child: Column(
-              children: List.generate(
-                listTileItems.length,
-                (index) => ListTile(
-                  contentPadding: EdgeInsets.zero,
-                  title: Text(
-                    listTileItems[index].title,
-                    style: AppStyles.bold13.copyWith(
-                      color: const Color(0xFF949D9E),
-                    ),
+          Column(
+            children: List.generate(
+              listTileItems.length,
+              (index) => ListTile(
+                contentPadding: EdgeInsets.zero,
+                title: Text(
+                  listTileItems[index].title,
+                  style: AppStyles.bold13.copyWith(
+                    color: const Color(0xFF949D9E),
                   ),
-                  leading: listTileItems[index].trailing,
-                  trailing: listTileItems[index].leading,
                 ),
+                leading: listTileItems[index].leading,
+                trailing: listTileItems[index].trailing,
               ),
             ),
+          ),
+          const SizedBox(height: 22),
+          Text(
+            'المساعده',
+            style: AppStyles.semiBold13.copyWith(
+              color: const Color(0xFF0C0D0D),
+            ),
+          ),
+          const SizedBox(height: 16),
+          ListTile(
+            contentPadding: EdgeInsets.zero,
+            title: Text(
+              'من نحن',
+              style: AppStyles.semiBold13.copyWith(
+                color: const Color(0xFF949D9E),
+              ),
+            ),
+            leading: Icon(
+              Iconsax.info_circle_copy,
+              color: ColorData.kPrimaryColor,
+              size: 20,
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios, size: 20),
           ),
         ],
       ),
