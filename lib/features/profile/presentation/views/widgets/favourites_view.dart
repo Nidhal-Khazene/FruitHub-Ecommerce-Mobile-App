765853d4d1a41c0a9c0eb6834634518ec755c763
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-19 09:23:23 +0100

    favourites view ui doen

--- lib/features/profile/presentation/views/widgets/favourites_view.dart
+++ lib/features/profile/presentation/views/widgets/favourites_view.dart
@@ -1,20 +1,35 @@
 import 'package:ecommerce_app/constants.dart';
 import 'package:ecommerce_app/core/widgets/app_bar_with_back_arrow.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class FavouritesView extends StatelessWidget {
+import '../../../../../core/cubits/products_cubit/products_cubit.dart';
+import '../../../../../core/repos/products_repo/products_repo.dart';
+import '../../../../../core/services/service_locator.dart';
+import '../../../../home/presentation/views/widgets/products_grid_view_bloc_builder.dart';
+
+class FavouritesView extends StatefulWidget {
   const FavouritesView({super.key});
 
   static const String routeName = "FavouritesView";
 
   @override
+  State<FavouritesView> createState() => _FavouritesViewState();
+}
+
+class _FavouritesViewState extends State<FavouritesView> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBarWithBackArrow(context, title: "المفضلة"),
-      body: const SafeArea(
-        child: Padding(
-          padding: kPrimaryScreenPadding,
-          child: SingleChildScrollView(child: Column()),
+    return BlocProvider(
+      create: (context) =>
+          ProductsCubit(getIt<ProductsRepo>())..getBestSellingProducts(),
+      child: Scaffold(
+        appBar: buildAppBarWithBackArrow(context, title: "المفضلة"),
+        body: const SafeArea(
+          child: Padding(
+            padding: kPrimaryScreenPadding,
+            child: CustomScrollView(slivers: [ProductsGridViewBlocBuilder()]),
+          ),
         ),
       ),
     );
