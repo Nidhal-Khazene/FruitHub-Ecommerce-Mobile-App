b4555e19b7a6c34b12af83586af4204349bec862
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-21 10:58:17 +0100

    add item entity

--- lib/features/checkout/domain/entities/paypal_payment_entity/item.dart
+++ lib/features/checkout/domain/entities/paypal_payment_entity/item.dart
@@ -1,3 +1,6 @@
+import 'package:ecommerce_app/core/helper/get_currency.dart';
+import 'package:ecommerce_app/features/cart/domain/entities/cart_item_entity.dart';
+
 class Item {
   String? name;
   int? quantity;
@@ -6,12 +9,14 @@ class Item {
 
   Item({this.name, this.quantity, this.price, this.currency});
 
-  factory Item.fromJson(Map<String, dynamic> json) => Item(
-    name: json['name'] as String?,
-    quantity: json['quantity'] as int?,
-    price: json['price'] as String?,
-    currency: json['currency'] as String?,
-  );
+  factory Item.fromEntity(CartItemEntity cartItemEntity) {
+    return Item(
+      currency: getCurrency(),
+      name: cartItemEntity.productEntity.productName,
+      price: cartItemEntity.productEntity.productPrice.toString(),
+      quantity: cartItemEntity.quantity,
+    );
+  }
 
   Map<String, dynamic> toJson() => {
     'name': name,
--- lib/features/checkout/domain/entities/paypal_payment_entity/item_list.dart
+++ lib/features/checkout/domain/entities/paypal_payment_entity/item_list.dart
@@ -5,12 +5,6 @@ class ItemList {
 
   ItemList({this.items});
 
-  factory ItemList.fromJson(Map<String, dynamic> json) => ItemList(
-    items: (json['items'] as List<dynamic>?)
-        ?.map((e) => Item.fromJson(e as Map<String, dynamic>))
-        .toList(),
-  );
-
   Map<String, dynamic> toJson() => {
     'items': items?.map((e) => e.toJson()).toList(),
   };
